29f26f6914af0e8b417acaa4d7bd80e8f58939e5
spandansenchoudhury <Spandan833@gmail.com>
2024-05-31 22:44:33 +0530

    basic in memory login

--- src/main/java/com/springframework/springrecipeapp/SpringRecipeAppApplication.java
+++ src/main/java/com/springframework/springrecipeapp/SpringRecipeAppApplication.java
@@ -2,8 +2,9 @@ package com.springframework.springrecipeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SpringRecipeAppApplication {
 
 	//Spandan 26/5/24
